db320f1c7bb075335d8532a2eaa5f28108bc3523
lewis-muema <lewis@sendy.co.ke>
2024-01-03 16:41:55 +0300

    Design tracks page

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import {
-  View, StyleSheet, Text, Button, SafeAreaView,
+  View, StyleSheet, Text, Button, SafeAreaView, ImageBackground,
 } from 'react-native';
+import { useFonts } from 'expo-font';
 import { useNavigation } from '@react-navigation/native';
 import { Context as AuthContext } from '../context/AuthContext';
 
@@ -15,18 +16,59 @@ const TrackListScreen = () => {
     validateAuth();
   }, []);
 
-  return <SafeAreaView>
-    <View>
-      <Text style={styles.title}>Hi</Text>
-      <Button title='Track details' onPress={() => navigation.navigate('TrackDetail')} />
-    </View>
-  </SafeAreaView>;
+  const [fontsLoaded] = useFonts({
+    manuscript: require('../../assets/fonts/Manuscript.ttf'),
+  });
+
+  return <ImageBackground source={require('../../assets/bg4.png')} resizeMode="cover" style={styles.bg}>
+    <SafeAreaView>
+        <View style={styles.container}>
+          { fontsLoaded ? <View style={styles.logo}>
+            <Text style={styles.title}>TRAILS</Text>
+            <Text style={styles.superscript}>MD</Text>
+          </View> : null }
+          <Button title='Track details' onPress={() => navigation.navigate('TrackDetail')} />
+        </View>
+    </SafeAreaView>
+  </ImageBackground>;
 };
 
 const styles = StyleSheet.create({
   title: {
-    textAlign: 'center',
+    textAlign: 'left',
     fontWeight: '600',
+    fontFamily: 'manuscript',
+    fontSize: 60,
+    color: '#faeed9',
+    textShadowColor: '#171717',
+    textShadowOffset: { width: -2, height: 4 },
+    textShadowRadius: 3,
+    paddingHorizontal: 5,
+  },
+  superscript: {
+    textAlign: 'left',
+    fontFamily: 'manuscript',
+    color: '#faeed9',
+    textShadowColor: 'black',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 3,
+    letterSpacing: 2,
+  },
+  logo: {
+    flexDirection: 'row',
+  },
+  trailsLogo: {
+    width: 200,
+    height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'flex-start',
+    marginBottom: 20,
+  },
+  container: {
+    marginHorizontal: 20,
+  },
+  bg: {
+    height: '100%',
   },
 });
 
